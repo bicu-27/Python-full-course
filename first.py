9264cd484e2dca1302ad6fa2f9abb3edee8cfa69
Lydia Kebede <netsaneta757@gmail.com>
2025-03-26 23:14:05 +0300

    Add files via upload
    Python from the very beginning

---
+++ first.py
@@ -0,0 +1,25 @@
+# Python is high level programming language.
+# High level meaning  it is easy for human to read and write.
+print("geeks quiz")
+ #Basics of python
+ #Python code is interpreted not compiled. Meaning python code is ted line by line.
+ #How python works
+#First we have source code  then the source code converted into bytecode then interpreted by python virtual machine.
+# Variables in python
+# variables are case sensative in python and they are used to store data.
+b = 3
+B = 4
+print(b)
+# Expression in python
+# Expression is a combination of values, variables and operators.
+# Expression is evaluated to give a result.
+a = 2
+b = 4
+c = a + b
+print(c)
+# Condition in python
+# Used to evaluate if something is true otr false.
+# if statement or basic conditiom
+age = 21
+if age >= 20:
+    print("You are old enough to drive")
